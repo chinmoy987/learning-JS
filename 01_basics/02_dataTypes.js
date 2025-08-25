ba099ff323ce854ef8b6dd713a3b898e58fc39b2
Chinmoy Biswas <chinmoybiswas488@gmail.com>
2025-08-25 17:42:44 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all Js code as newer versions
+
+//alert(3 + 3)    // we are using nodeJs,not browser
+
+console.log(
+  3 + 3 // always maintain high code readablity,so don't do this
+);
+
+let fullName = "chinmoy";
+let age = 20;
+let isLogedIn = false;
+
+// Primitive datatypes--------------------------------------------------------------------------------
+// number=> 2 to the power 53 is the range
+// bigint=> for bigger numbers
+// for strings => " "
+// boolean => true/false
+// null => standalone value (empty) (datatype is object)
+// undefing => value is not initialized yet
+// symbol => unique
+
+console.log(typeof fullName);
